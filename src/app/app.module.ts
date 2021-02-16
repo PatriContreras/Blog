dc3a75276d3582e74e7822c90e65e0bae05a5a0c
PatriContreras <patri.contreras91@gmail.com>
2021-02-16 21:50:57 +0100

    Instalado Bootstrap, creado Routing inicial, creado interfaz Post

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
